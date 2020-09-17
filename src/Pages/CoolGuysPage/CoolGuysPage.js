5e0e2904e8b1baca64bb29efa1f4914e58f604dd
moshe vilner <shootermv@gmail.com>
2020-09-17 19:09:56 +0300

    feat: adding remove-guy button

--- src/Pages/CoolGuysPage/CoolGuysPage.js
+++ src/Pages/CoolGuysPage/CoolGuysPage.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './CoolGuys.css'
+import './CoolGuys.css';
+
+const removePerson = (id, setUsers) => {
+  let coolGuys = localStorage.getItem("coolGuys")
+    ? JSON.parse(localStorage.getItem("coolGuys"))
+    : [];
+  const filtered = coolGuys.filter(({id: _id}) => _id !== id);
+
+  localStorage.setItem("coolGuys", JSON.stringify(filtered));
+  setUsers(filtered);
+};
+
 export default function CoolGuysPage() {
     const [users, setUsers] = useState(localStorage.getItem('coolGuys') ? JSON.parse(localStorage.getItem('coolGuys')) : [])
+    
     return (
         <>
           <h1>Cool guys you  following</h1>
@@ -11,6 +23,7 @@ export default function CoolGuysPage() {
                  <span><Link to={`/author/${login}/${email}`}>{name}</Link></span>
                  <img alt="" src={avatar_url} />
                  <span>{repo}</span>
+                 <button onClick={() => removePerson(id, setUsers)}>unfollow</button>
                </li>))} 
           </ul> : <>no cool guys yet...</>}
         </>
