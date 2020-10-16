c23148863035d5610faf8b04ed4fae0ec9db3a1c
mosheZ900 <shootermv@gmail.com>
2020-10-16 12:53:14 +0300

    chore: storybook support added

---
+++ src/shared/Spinner.stories.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Spinner from './Spinner';
+export default {
+    title: 'Spinner',
+    component: Spinner,
+  };
+
+
+export const SpinnerStory = () => (
+ 
+         <Spinner />
+
+);
