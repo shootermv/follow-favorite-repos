2e823251071558c6e822ba8734b6a4c5641633df
mosheZ900 <shootermv@gmail.com>
2020-10-21 09:52:47 +0300

    test: adding dynamic tabs stories

--- src/shared/Tabs/Tabs.stories.js
+++ src/shared/Tabs/Tabs.stories.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
+
 import Tab from './Tab';
 import TabPanel from './TabPanel';
 import Tabs from './Tabs';
 
 export default {
-  title: 'Tabs',
+  title: 'Tabs - static',
   component: Tabs,
 };
 
-export const TabsStory = () => (
+export const StaticTabs = () => (
   <Tabs>
     <Tab key="tab1">Tab 1</Tab>
     <Tab key="tab2">Tab 2</Tab>
@@ -22,3 +23,26 @@ export const TabsStory = () => (
     </TabPanel>
   </Tabs>
 );
+
+export const DynamicTabs = () => {
+
+  const tabs = [
+    { tabName: "tab 1", component: () => <>123</> },
+    { tabName: "tab 2", component: () => <>456</> },
+    { tabName: "tab 3", component: () => <>789</> }
+  ];
+
+  return (
+      <Tabs>
+        {tabs.map(({ tabName }) => (
+          <Tab key={tabName}>{tabName}</Tab>
+        ))}
+
+        {tabs.map(({ component: My, tabName }) => (
+          <TabPanel key={`panel-${tabName}`}>
+            <My/>
+          </TabPanel>
+        ))}
+      </Tabs>
+);
+}
