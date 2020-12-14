9486427bafbdcd7ba1a337019f13b6ab1be27d86
moshe vilner <shootermv@gmail.com>
2020-12-14 12:52:07 +0200

    test(e2e): add check for active tab

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+context('Home Page', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000')
+    })
+  
+    it('should have 3 tabs', () => {
+      cy.get('[data-test-id="tab-title"]')
+      .should('have.length', 3)
+    })
+  
+    it('initially "commits" table should be active', () => {
+      cy.get('[data-test-id="tab-title"]').get('button').first()
+      .should('have.class', 'active')
+      cy.get('[data-test-id="tab-title"]').get('button').eq(1).should('not.have.class', 'active')
+
+    })    
+  })
+  
